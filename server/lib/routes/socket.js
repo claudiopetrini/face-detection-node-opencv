f342dd4897fbb8a142f708bb2aa5f54e3ec879d4
Claudio Petrini <claudio.petrini@stamplay.com>
2015-05-24 14:38:00 +0200

    Updated libs, face and eyes detection

--- server/lib/routes/socket.js
+++ server/lib/routes/socket.js
@@ -1,14 +1,11 @@
-var cv = require('opencv');
+var cv = require('opencv'),
+  async = require('async');
 
 // camera properties
-var camWidth = 320;
-var camHeight = 240;
+var camWidth = 640;
+var camHeight = 480;
 var camFps = 10;
 var camInterval = 1000 / camFps;
-
-// face detection properties
-var rectColor = [0, 255, 0];
-var rectThickness = 2;
 
 // initialize camera
 var camera = new cv.VideoCapture(0);
@@ -16,20 +13,46 @@ camera.setWidth(camWidth);
 camera.setHeight(camHeight);
 
 module.exports = function (socket) {
-  setInterval(function() {
-    camera.read(function(err, im) {
-      if (err) throw err;
-
-      im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
-        if (err) throw err;
-
-        for (var i = 0; i < faces.length; i++) {
-          face = faces[i];
-          im.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], rectColor, rectThickness);
-        }
-
-        socket.emit('frame', { buffer: im.toBuffer() });
-      });
-    });
+  setInterval(function () {
+    async.auto({
+      readFromCamera: readFromCamera,
+      face: ['readFromCamera', detect(cv.FACE_CASCADE)],
+      eyes: ['readFromCamera', detect('./node_modules/opencv/data/haarcascade_mcs_eyepair_small.xml')]
+    }, emitFrame(socket));
   }, camInterval);
 };
+
+var readFromCamera = function (callback) {
+  camera.read(function (err, im) {
+    if (err) callback(err);
+    callback(null, im);
+  });
+}
+
+var detect = function (haarfile) {
+  return function (callback, results) {
+    var im = results['readFromCamera'];
+    im.detectObject(haarfile, {}, function (err, faces) {
+      if (err) callback(err);
+
+      for (var i = 0; i < faces.length; i++) {
+        face = faces[i];
+        im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
+      }
+      callback(null, im);
+    });
+  }
+}
+
+var emitFrame = function (socket) {
+  return function (err, results) {
+    if (err) {
+
+    } else {
+      var im = results['eyes'];
+      socket.emit('frame', {
+        buffer: im.toBuffer()
+      });
+    }
+  }
+}
